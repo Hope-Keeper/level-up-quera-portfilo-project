8417f22a1450d583fb83cd44cd91d45f24c7fb36
Reihaneh <reyhaneh.horri@gmail.com>
2023-10-17 01:47:31 -0700

    before social media linkes

--- js/index.js
+++ js/index.js
@@ -1,13 +1,22 @@
-var taggleBtn = document.querySelector("#nav-menu-toggler");
-var menu = document.querySelector("#nav-menu-container");
-taggleBtn.addEventListener("click", function clickHandler() {
-  console.log("jgvhkgckhg");
-  if (menu.style.display == "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-  }
-});
+const taggleBtn = document.querySelector("#nav-menu-toggler");
+const menu = document.querySelector("#nav-menu-container");
+const navUl = document.querySelector("#nav-menu-list");
+const opensvg = document.querySelector(".open");
+const closesvg = document.querySelector(".close");
+// taggleBtn.addEventListener("click", function clickHandler() {
+//   console.log("jgvhkgckhg");
+//   if (menu.className == "") {
+//     menu.className = "collapse1";
+//     navUl.className = "collapse2";
+//     opensvg.style.display = "block";
+//     closesvg.style.display = "none";
+//   } else {
+//     menu.className = "";
+//     navUl.className = "";
+//     opensvg.style.display = "none";
+//     closesvg.style.display = "block";
+//   }
+// });
 
 // function clickHandler() {
 //   console.log(menu);
@@ -15,3 +24,38 @@ taggleBtn.addEventListener("click", function clickHandler() {
 //   console.log("jhfh");
 //   menu.style.display = "block";
 // }
+
+taggleBtn.addEventListener("click", function clickHandler() {
+  console.log("jgvhkgckhg");
+  menu.classList.toggle("collapse1");
+  navUl.classList.toggle("collapse2");
+
+  if (navUl.className == "nav-menu-list collapse2") {
+    opensvg.style.display = "none";
+
+    closesvg.style.display = "block";
+  } else {
+    opensvg.style.display = "block";
+
+    closesvg.style.display = "none";
+  }
+
+  // if (navUl.className == "collapse2") {
+  //   navUl.className = "nav-menu-list";
+  // } else {
+  //   navUl.className = "collapse2";
+  //   menu.className = "collapse1";
+  // }
+
+  // if (menu.className == "") {
+  //   menu.className = "collapse1";
+  //   navUl.className = "collapse2";
+  //   opensvg.style.display = "block";
+  //   closesvg.style.display = "none";
+  // } else {
+  //   menu.className = "";
+  //   navUl.className = "";
+  //   opensvg.style.display = "none";
+  //   closesvg.style.display = "block";
+  // }
+});
